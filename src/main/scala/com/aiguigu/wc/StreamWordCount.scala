d3e6e00e8300220f5ab38703a25ae524f859736b
liuyang <liuyang@coohua.com>
2020-09-28 08:54:58 +0800

    一些可以配置的部分修改

--- src/main/scala/com/aiguigu/wc/StreamWordCount.scala
+++ src/main/scala/com/aiguigu/wc/StreamWordCount.scala
@@ -1,5 +1,6 @@
 package com.aiguigu.wc
 
+import org.apache.flink.api.java.utils.ParameterTool
 import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
 import org.apache.flink.streaming.api.scala._
 
@@ -7,14 +8,21 @@ object StreamWordCount {
   def main(args: Array[String]): Unit = {
     val env = StreamExecutionEnvironment.getExecutionEnvironment
 
-    val stream = env.socketTextStream("localhost", 7777)
+    // 可以突破核心数
+    // env.setParallelism(24)
+
+    val paramTool: ParameterTool = ParameterTool.fromArgs(args)
+    val host: String = paramTool.get("host")
+    val port: Int = paramTool.getInt("port")
+
+    val stream = env.socketTextStream(host, port)
     val resultDataStream = stream
       .flatMap(_.split(" "))
       .filter(_.nonEmpty)
       .map((_, 1))
       .keyBy(0)
       .sum(1)
-    resultDataStream.print()
+    resultDataStream.print().setParallelism(1)
     env.execute("StreamWorldCount")
   }
 }
