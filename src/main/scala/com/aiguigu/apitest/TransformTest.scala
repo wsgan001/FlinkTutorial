9e20e1cc9be430646e8b3f934fb0724727211d13
liuyang <liuyang@coohua.com>
2020-10-01 15:51:43 +0800

    Transform 多流转换-connect union

--- src/main/scala/com/aiguigu/apitest/TransformTest.scala
+++ src/main/scala/com/aiguigu/apitest/TransformTest.scala
@@ -21,13 +21,13 @@ object TransformTest {
     // 4. keyBy 定义两个任务间数据传输的模式 DataStream -> KeyedStream
     // 5. 滚动聚合算子: sum/min/max/minBy/maxBy
     val env = StreamExecutionEnvironment.getExecutionEnvironment
-//    env.setParallelism(1)
+    //    env.setParallelism(1)
     // 0. 读取数据
     val inputPath = getClass.getResource("/sensor.txt").getPath
     val inputStream = env.readTextFile(inputPath)
     // 1. 先转换成SensorReading数据类型(简单转换操作)
     val dataStream = inputStream.map(
-      data=>{
+      data => {
         val arr = data.split(",")
         SensorReading(arr(0), arr(1).toLong, arr(2).toDouble)
       }
@@ -43,29 +43,44 @@ object TransformTest {
       .reduce((curState, newData) => {
         SensorReading(curState.id, newData.timestamp, curState.temperature.min(newData.temperature))
       })
-//      .reduce(new MyReduceFunction())
+    //      .reduce(new MyReduceFunction())
+    //    resultStream.print()
 
     // DataStream -> split -> SplitStream -> select -> DataStream
     // 4. 多流转换操作
     // 4.1 将传感器数据分为低温和高温两个流
     val splitStream = dataStream
-      .split( data => {
+      .split(data => {
         if (data.temperature > 30) Seq("high") else Seq("low")
       })
 
     val highTempStream = splitStream.select("high")
-    val lowTempStream  = splitStream.select("low")
-    val allTempStream  = splitStream.select("high", "low")
-    highTempStream.print("high")
-    lowTempStream.print("low")
-    allTempStream.print("all")
+    val lowTempStream = splitStream.select("low")
+    val allTempStream = splitStream.select("high", "low")
+//    highTempStream.print("high")
+//    lowTempStream.print("low")
+//    allTempStream.print("all")
 
-//    resultStream.print()
+    // 4.2. 合流操作connect
+    val warningStream = highTempStream.map(data => (data.id, data.temperature))
+
+    val connectedStreams = warningStream.connect(lowTempStream)
+    // 使用CoMap对数据进行分别处理
+
+    val coMapResultStream = connectedStreams
+      .map(
+        warningData => (warningData._1, warningData._2, "warning"),
+        lowTempData => (lowTempData.id, "healthy"))
+
+    coMapResultStream.print()
+
+    // 4.3 union 合流 要求两个流的数据类型相同
+
     env.execute("TransformTest")
   }
 }
 
-class MyReduceFunction extends ReduceFunction [SensorReading] {
+class MyReduceFunction extends ReduceFunction[SensorReading] {
   override def reduce(v1: SensorReading, v2: SensorReading): SensorReading = {
     SensorReading(v1.id, v2.timestamp, v1.temperature.min(v2.temperature))
   }
