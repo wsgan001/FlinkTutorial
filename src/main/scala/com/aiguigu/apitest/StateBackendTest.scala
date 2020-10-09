092d164049c155b390423911d524a6faa2aee4e0
liuyang <liuyang@coohua.com>
2020-10-09 15:43:22 +0800

    checkpoint的一些配置

--- src/main/scala/com/aiguigu/apitest/StateBackendTest.scala
+++ src/main/scala/com/aiguigu/apitest/StateBackendTest.scala
@@ -11,5 +11,22 @@ object StateBackendTest {
     env.setStateBackend(new FsStateBackend("uri"))
     env.setStateBackend(new MemoryStateBackend())
     env.setStateBackend(new RocksDBStateBackend(""))
+
+    // 打开检查点
+    env.enableCheckpointing(1000)
+    //    env.getCheckpointConfig.setCheckpointingMode()
+    //    env.getCheckpointConfig.setCheckpointTimeout(60 * 1000)
+    // 最多允许多少个checkpoint在进行
+    env.getCheckpointConfig.setMaxConcurrentCheckpoints(2)
+
+    // 没两个checkpoint间最小的间隔时间, 防止没有时间做真正的计算
+    env.getCheckpointConfig.setMinPauseBetweenCheckpoints(60000)
+
+    // 是否倾向于用checkpoint做故障恢复
+    // 如果有一个比checkpoint更近的savepoint 那么用哪个?
+    // 不配也罢
+    env.getCheckpointConfig.setPreferCheckpointForRecovery(true)
+    // 做checkpoint失败允许的次数
+    env.getCheckpointConfig.setTolerableCheckpointFailureNumber(2)
   }
 }
