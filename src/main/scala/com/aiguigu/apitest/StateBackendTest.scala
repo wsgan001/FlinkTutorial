c60ed68aaf549e64b824cda6f0595550ee760519
liuyang <liuyang@coohua.com>
2020-10-09 15:51:43 +0800

    重启策略

--- src/main/scala/com/aiguigu/apitest/StateBackendTest.scala
+++ src/main/scala/com/aiguigu/apitest/StateBackendTest.scala
@@ -1,5 +1,7 @@
 package com.aiguigu.apitest
 
+import org.apache.flink.api.common.restartstrategy.RestartStrategies
+import org.apache.flink.api.common.time.Time
 import org.apache.flink.contrib.streaming.state.RocksDBStateBackend
 import org.apache.flink.runtime.state.filesystem.FsStateBackend
 import org.apache.flink.runtime.state.memory.MemoryStateBackend
@@ -28,5 +30,13 @@ object StateBackendTest {
     env.getCheckpointConfig.setPreferCheckpointForRecovery(true)
     // 做checkpoint失败允许的次数
     env.getCheckpointConfig.setTolerableCheckpointFailureNumber(2)
+
+    // 任务重启策略
+    // 1. failureRateRestart
+    // 2. fallbackRestart
+    // 3. fixDelayRestart
+    // 4. noRestart
+    env.setRestartStrategy(RestartStrategies.fixedDelayRestart(3, 1000))
+    env.setRestartStrategy(RestartStrategies.failureRateRestart(3, Time.minutes(5), Time.seconds(10)))
   }
 }
