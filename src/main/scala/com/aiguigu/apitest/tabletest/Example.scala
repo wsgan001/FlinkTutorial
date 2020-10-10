a70cd3aec591f5898cf3262f1c1d6a69ce755f2d
liuyang <liuyang@coohua.com>
2020-10-10 21:46:03 +0800

    flink sql

---
+++ src/main/scala/com/aiguigu/apitest/tabletest/Example.scala
@@ -0,0 +1,38 @@
+package com.aiguigu.apitest.tabletest
+
+import com.aiguigu.apitest.SensorReading
+import org.apache.flink.streaming.api.scala.{StreamExecutionEnvironment, _}
+import org.apache.flink.table.api.Table
+import org.apache.flink.table.api.scala.{StreamTableEnvironment, _}
+
+object Example {
+  def main(args: Array[String]): Unit = {
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+    env.setParallelism(1)
+    val inputPath = getClass.getResource("/sensor.txt").getPath
+    val inputStream = env.readTextFile(inputPath)
+    val dataStream = inputStream
+      .map(data => {
+        val arr = data.split(",")
+        SensorReading(arr(0), arr(1).toLong, arr(2).toDouble)
+      })
+
+    val tableEnv = StreamTableEnvironment.create(env)
+
+    val dataTable: Table = tableEnv.fromDataStream(dataStream)
+
+    val resultTable = dataTable
+      .select("id, temperature")
+      .filter("id == 'sensor_1'")
+    resultTable.toAppendStream[(String, Double)]
+        .print()
+
+    // 用sql实现
+    tableEnv.createTemporaryView("dataTable", dataTable)
+    val sql = "SELECT id,temperature FROM dataTable WHERE id='sensor_1'"
+    val resultSqlTable = tableEnv.sqlQuery(sql)
+    resultSqlTable.toAppendStream[(String, Double)].print("result")
+
+    env.execute("TableApiExample")
+  }
+}
